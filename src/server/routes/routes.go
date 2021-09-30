961f6bfa4086308640e7c087d10ef84aaeed4f85
Victor Bernardes da Silva Santos <victor.bernardes.tc@take.net>
2021-09-30 12:59:39 -0300

    adding routes to product without controllers

--- src/server/routes/routes.go
+++ src/server/routes/routes.go
@@ -9,11 +9,25 @@ import (
 func ConfigRoutes(r *fiber.App) *fiber.App {
 	router := r.Group("api/v1")
 	{
-		router.Get("/", controllers.ListAllUsers)
-		router.Post("/create", controllers.CreateUser)
-		router.Put("/update/id::id", controllers.UpdateUser)
-		router.Patch("/updatepass/id::id", controllers.UpdateUserPassword)
-		router.Delete("/delete/id::id", controllers.Delete)
+		router.Group("user")
+		{
+			router.Get("/", controllers.ListAllUsers)
+			router.Post("/create", controllers.CreateUser)
+			router.Put("/update/id::id", controllers.UpdateUser)
+			router.Patch("/updatepass/id::id", controllers.UpdateUserPassword)
+			router.Delete("/delete/id::id", controllers.Delete)
+		}
+
+		router.Group("product")
+		{
+			router.Get("/all")
+			router.Get("/getbyid/id::id")
+			router.Get("/getbytitle/title::title")
+			router.Post("/create")
+			router.Put("/update/id::id")
+			router.Get("/delete/id::id")
+		}
+
 	}
 
 	return r
